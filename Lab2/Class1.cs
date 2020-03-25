2757c27d3d4aa21e921ca384cef24426d2bac107
Math2345 <maxim.hramtsoff@gmail.com>
2020-03-26 00:00:05 +0300

    V1.0

---
+++ Lab2/Class1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Complex
+    {
+        double re, im;
+
+        public Complex(double r, double i)
+        {
+            re = r;
+            im = i;
+        }
+
+
+        public static Complex sum(Complex C1, Complex C2)
+        {
+            return new Complex(C1.re + C2.re, C1.im + C2.im);
+        }
+
+        public static string ToString(Complex C1)
+        {
+            if (C1.im>=0)
+                return "" + C1.re + "+" + C1.im + "i";
+            else 
+                return "" + C1.re + C1.im + "i";
+
+        }
+
+    }
+
+    public class Desk
+    {
+        public int n, xAttacker, yAttacker, xf, yf, figure;
+
+        public Desk(int nn, int xA, int yA, int xxf, int yyf, int ffigure)
+        {
+            n = nn;
+            xAttacker = xA;
+            yAttacker = yA;
+            xf = xxf;
+            yf = yyf;
+            figure = ffigure;
+        }
+        // Количество Клеток на доске
+        public static int Count(Desk C1)
+        {
+            return C1.n * C1.n;
+        }
+        //Цвет клетки
+        public static string Color(int k, int m)
+        {
+            if ((k + m) % 2 == 0) return "black"; else return "white";
+
+        }
+        //Одинаковый цвет
+        public static string SameSquare(int k1, int m1, int k2, int m2)
+        {
+            if (((k1 + m1) % 2 == (k2 + m2) % 2)) return "Same"; else return "Different";
+        }
+
+        public static string EatPown(int xA, int yA, int xf, int yf)
+        {
+            if (((xf == xA - 1) || (xf == xA + 1)) && (yf == xA - 1)) { return "Eat"; } else { return "No eat"; };
+
+        }
+
+        public static string EatEleph(int xEleph, int yEleph, int xf, int yf)
+        {
+            if (((xf - xEleph) == (yf - yEleph)) || ((xf - xEleph) == (-yf + xEleph))) { return "Eat"; } else { return "No eat"; }
+        }
+
+
+        public static string EatTower(int xTower, int yTower, int xf, int yf)
+        {
+            if ((xTower == xf) || (yTower == yf))
+            return "Eat"; 
+            else  return "No eat"; 
+        }
+        public static string EatQueen(int xQueen, int yQueen, int xf, int yf)
+        {
+            if ( ( ((xf - xQueen) == (yf - yQueen)) || ((xf - xQueen) == (-yf + yQueen))) ||// диагонали
+                ((xQueen == xf) || (yQueen == yf)) )// вертикаль , горизонталь
+            return "Eat"; 
+            else  return "No eat"; 
+
+        }
+
+        public static string EatHorse(int xHorse, int yHorse, int xf, int yf)
+        {
+            if (((xHorse - 1 == xf) && (yHorse - 2 == yf)) || ((xHorse - 1 == xf) && (yHorse + 2 == yf)) || // 8 позиций атаки коня 
+                ((xHorse + 1 == xf) && (yHorse - 2 == yf)) || ((xHorse + 1 == xf) && (yHorse + 2 == yf)) ||
+                ((xHorse - 2 == xf) && (yHorse - 1 == yf)) || ((xHorse - 2 == xf) && (yHorse + 1 == yf)) ||
+                ((xHorse + 2 == xf) && (yHorse - 1 == yf)) || ((xHorse + 2 == xf) && (yHorse + 1 == yf))) 
+             return "Eat"; 
+            else  return "No eat"; 
+
+        }
+
+        //Фигура под ударом
+        public static void Eating(Desk C1)
+        {
+            switch (C1.figure)
+            {
+                case 1: // пешка
+                    Console.WriteLine("Pown " + EatPown(C1.xAttacker, C1.yAttacker, C1.xf, C1.yf));
+                    break;
+                case 2: // Слон
+                    Console.WriteLine("Elephant " + EatEleph(C1.xAttacker, C1.yAttacker, C1.xf, C1.yf));
+                    break;
+                case 3: // Ладья
+                    Console.WriteLine("Tower " + EatTower(C1.xAttacker, C1.yAttacker, C1.xf, C1.yf));
+                    break;
+                case 4: // Ферзь
+                    Console.WriteLine("Queen " + EatQueen(C1.xAttacker, C1.yAttacker, C1.xf, C1.yf));
+                    break;
+                case 5: // Конь
+                    Console.WriteLine("Horse " + EatHorse(C1.xAttacker, C1.yAttacker, C1.xf, C1.yf));
+                    break;
+
+                default:
+                    Console.WriteLine("There is no such figure");
+                    break;
+            }
+        }
+
+    }
+
+    public class Date
+    {
+        
+        public static string daysFromStart(DateTime d1)
+        {
+            DateTime d2 = new DateTime(d1.Year,1,1);
+            int days = (d1 - d2).Days;
+            return "Passed " + days +" days";
+        }
+
+        public static string daysToEnd(DateTime d1)
+        {
+            DateTime d2 = new DateTime(d1.Year, 12, 31);
+            int days = (d2 - d1).Days;
+            return "Left "+ days + " days";
+        }
+    }
+
+    public class Armstrong
+    {
+
+        public static void armstrong()
+        {
+            for(int i = 100; i < 1000; i++)
+            {
+                int x = i / 100;
+                int y = i % 100 / 10;
+                int z = i % 10;
+                int q = (int)Math.Pow(x, 3) + (int)Math.Pow(y, 3) + (int)Math.Pow(z, 3);
+                if (q == i) Console.WriteLine("Armstrong's number - "+ q);
+            }
+        }
+
+    }
+
+    public class Square
+    {
+        public static int Squares(int N, int M)
+        {
+            if (N == 0 || M == 0) return 0;
+            if (N == M) return 1;
+            int min = Math.Min(N, M);
+            int max = Math.Max(N, M);
+            return 1 + Squares(min, max - min);
+        }
+    }
+
+    public class RightTriangle
+    {
+        public static double Area(double p)
+        {
+            double a = p / 3;
+            return Math.Sqrt(3)/4*a*a ;
+        }
+    }
+
+    public class EvenNumber
+    {
+        public static String Check(int x)
+        {
+            if (x % 2 == 0)
+                return "even";
+            else return "odd";   
+        }
+    }
+
+
+    public class Figure
+    {
+        public static void FigureCheck()
+        {
+            Console.WriteLine("Фигура");
+             Console.Write("x=");            
+             float x = float.Parse(Console.ReadLine());
+             Console.Write("y=");
+             float y = float.Parse(Console.ReadLine());
+
+             if (x<0 && y<23 && y>0 && x+y>0)
+                 Console.WriteLine("внутри");
+             else if ((x + y == 0 && x >= -23 && x <= 0) || (x == 0 && y >= 0 && y <= 23) || (x >= -23 && x <= 0 && y == 23))
+                   Console.WriteLine("на границе");
+                    else Console.WriteLine("вне");
+        }
+    }
+
+    public class Months
+    {
+        public static void ToEndOfYear()
+        {
+            Console.WriteLine("Месяцы до конца года");   
+            Console.Write("n=");            
+            int n = int.Parse(Console.ReadLine());
+            switch (n)
+                      {
+            case 1: Console.WriteLine(12-1); break;
+            case 2: Console.WriteLine(12-2); break;
+            case 3: Console.WriteLine(12-3); break;
+            case 4: Console.WriteLine(12-4); break;
+            case 5: Console.WriteLine(12-5); break;
+            case 6: Console.WriteLine(12-6); break;
+            case 7: Console.WriteLine(12-7); break;
+            case 8: Console.WriteLine(12-8); break;
+            case 9: Console.WriteLine(12-9); break;
+            case 10: Console.WriteLine(12-10); break;
+            case 11: Console.WriteLine(12-11); break;
+            case 12: Console.WriteLine(12-12); break;
+            default: Console.WriteLine("ВЫ ОШИБЛИСЬ"); break;
+          }
+
+        }
+    }
+
+    //4.	таблицу перевода расстояний в дюймах в сантиметры для значений 2, 4, 6, …, 12 дюймов (1 дюйм = 25.4 мм);
+    public class Units
+    {
+        public static void Convert()
+        {
+            Console.WriteLine("Перевод дюйм в см ");
+            Console.Write("n=");            
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("while: ");
+            int i = 2;           
+            while (i <= n)
+            {
+                Console.Write(" " + i*2.54);
+                i += 2;
+            }
+
+            Console.WriteLine();
+            Console.Write("do while: ");
+            i = 2;
+            do
+            {
+                Console.Write(" " + i * 2.54);
+                i += 2;
+            }
+            while (i <= n);
+
+            Console.WriteLine();
+            Console.Write("For: ");
+            for (i = 2; i<=n; i+=2)
+            {
+                Console.Write(" " + i * 2.54);
+            }
+            Console.WriteLine();
+         }
+   }
+
+    public class TrianglePrint
+    {
+        public static void Print()
+        {
+            int k = 5;
+            int l = 3;
+            for (int i = 1; i<=10; ++i, Console.WriteLine())
+             {
+                 if (i % 2 != 0)
+                 {
+                     for (int j = 1; j <= k; ++j)
+                     Console.Write(" " + 2);
+                    
+                 }
+                 else
+                 {
+                     for (int j = 1; j <= k; ++j)
+                     {
+                        Console.Write(" ");
+                        Console.Write(l+j-1);
+                     }
+                     k--;
+                     l--;
+                 }
+
+             }
+        }
+   }
+}
+
+        
---
+++ Sharplab2/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab2;
+
+namespace Sharplab2
+{
+    class SharpLab2
+    {
+        static void Main(string[] args)
+        {
+            void PrintMenu()
+            {
+                Console.WriteLine();
+                Console.WriteLine("0. Выход");
+                Console.WriteLine("1. Комплексные числа");
+                Console.WriteLine("2. Шахматная доска");
+                Console.WriteLine("3. Даты");
+                Console.WriteLine("4. Числа Армстронга");
+                Console.WriteLine("5. Кол-во квадратов");
+                Console.WriteLine("6. К1 Треугоьник(В1)");
+                Console.WriteLine("7. К2 Проверка на четность");
+                Console.WriteLine("8. К3 Проверка принадлежности точки фигуре");
+                Console.WriteLine("9. К4 Дан порядковый номер месяца, вывести на экран количество месяцев оставшихся до конца года.");
+                Console.WriteLine("10. К5 Таблица конвертации дюймов в сантиметры");
+                Console.WriteLine("11. К6  Печать по шаблону");
+                Console.WriteLine();
+            }
+            PrintMenu();
+
+            int button = int.Parse(Console.ReadLine());
+            while (button != 0) //0 - exit
+            {
+                switch (button)
+                {
+                    case 1://Комплексные числа
+                        {
+                            Complex c1 = new Complex(1, 1);
+                            Complex c2 = new Complex(1, 1);
+                            Complex c3 = (Complex.sum(c1, c2));
+                            Console.WriteLine(Complex.ToString(c3));
+
+                            Complex c11 = new Complex(1, 1);
+                            Complex c21 = new Complex(1, -101);
+                            Complex c31 = (Complex.sum(c11, c21));
+                            Console.WriteLine(Complex.ToString(c31));
+                        }
+                        break;
+
+                    case 2://Доска
+                        {
+                            Console.WriteLine("1-готовые доски, 2-своя доска");
+                            int flag = int.Parse(Console.ReadLine());
+                            switch (flag)
+                            {
+                                case 1:
+                                    {
+                                        Desk Desk1 = new Desk(10, 2, 2, 3, 3, 1);   //Пешка +
+                                        Desk Desk1no = new Desk(10, 2, 2, 3, 2, 1); //Пешка -
+                                        Desk Desk2 = new Desk(10, 2, 2, 3, 3, 2);   //Слон +
+                                        Desk Desk2no = new Desk(10, 2, 1, 3, 3, 2); //Слон -
+                                        Desk Desk3 = new Desk(10, 2, 2, 2, 3, 3);   //Ладья +
+                                        Desk Desk3no = new Desk(10, 2, 2, 3, 3, 3); //Ладья -
+                                        Desk Desk4 = new Desk(10, 2, 2, 3, 3, 4);   //Ферзь +
+                                        Desk Desk4no = new Desk(10, 2, 2, 3, 4, 4); //Ферзь -
+                                        Desk Desk5 = new Desk(10, 2, 2, 4, 1, 5);   //Конь +
+                                        Desk Desk5no = new Desk(10, 2, 2, 4, 2, 5); //Конь -
+
+                                        Console.WriteLine("Количество клеток на доске со стороной " + Desk1.n + "=" + Desk.Count(Desk1));
+
+                                        Desk.Eating(Desk1);//Проверка атаки
+                                        Desk.Eating(Desk1no);
+                                        Desk.Eating(Desk2);
+                                        Desk.Eating(Desk2no);
+                                        Desk.Eating(Desk3);
+                                        Desk.Eating(Desk3no);
+                                        Desk.Eating(Desk4);
+                                        Desk.Eating(Desk4no);
+                                        Desk.Eating(Desk5);
+                                        Desk.Eating(Desk5no);
+                                    }
+                                    break;
+                                case 2:
+                                    {
+                                        Console.WriteLine("Введите размер стороны шахматной доски");
+                                        int size = int.Parse(Console.ReadLine());
+
+                                        Console.WriteLine("Введите тип фигуры 1-пешка, 2-слон, 3-Ладья, 4-Ферзь, 5-конь");
+                                        int fig = int.Parse(Console.ReadLine());
+                                       
+                                        Console.WriteLine("Введите координаты атакующей фигуры");
+                                        int xAtt = int.Parse(Console.ReadLine());
+                                        int yAtt = int.Parse(Console.ReadLine());
+                                        
+
+                                        Console.WriteLine("Введите координаты другой фигуры");
+                                        int xDef = int.Parse(Console.ReadLine());
+                                        int yDef = int.Parse(Console.ReadLine());
+
+                                        Desk DeskInput = new Desk(size, xAtt, yAtt, xDef, yDef, fig);
+                                        Console.WriteLine("Количество клеток на доске со стороной " + DeskInput.n + "=" + Desk.Count(DeskInput));
+
+                                        Desk.Eating(DeskInput);
+                                    }
+                                    break;
+                                default:
+                                    Console.WriteLine("Ошибка");
+                                    break;
+                            }
+
+                            Console.WriteLine("Цвет клетки: Введите 2 числа - координаты клетки");//Цвет клетки
+                            int k = int.Parse(Console.ReadLine());
+                            int m = int.Parse(Console.ReadLine());
+                            Console.WriteLine(Desk.Color(k, m));
+
+                            Console.WriteLine("Одинаковый цвет двух клеток :Введите 2 числа - координаты первой клетки");//Одинаковый цвет
+                            int k1 = int.Parse(Console.ReadLine());
+                            int m1 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Введите 2 числа - координаты второй клетки");
+                            int k2 = int.Parse(Console.ReadLine());
+                            int m2 = int.Parse(Console.ReadLine());
+                            Console.WriteLine(Desk.SameSquare(k1, m1, k2, m2));
+
+                        }
+                        break;
+
+                    case 3://Даты
+                        {
+                            DateTime d1 = new DateTime(2020, 02, 29);
+
+                            DateTime nowDate = DateTime.Now;
+                            string passedDays = Date.daysFromStart(d1); // Количество дней с даты d1
+                            Console.WriteLine(passedDays);
+
+                            string remainDays = Date.daysToEnd(d1); // Количество дней с даты d1 до конца года
+                            Console.WriteLine(remainDays);
+
+                            Console.WriteLine("Yesterday - {0}.{1}.{2}", (d1.AddDays(-1)).Year, (d1.AddDays(-1)).Month, (d1.AddDays(-1)).Day); //d-1
+                            Console.WriteLine("Tomorrow - {0}.{1}.{2}", (d1.AddDays(1)).Year, (d1.AddDays(1)).Month, (d1.AddDays(1)).Day);     //d+1
+                        }
+                        break;
+
+                    case 4://Числа Армстронга 1^3 + 5^3 + 3^3 = 153.
+                        {
+                            Armstrong.armstrong();
+                        }
+                        break;
+
+                    case 5: //Кол-во квадратов
+                        {
+                            Console.WriteLine("Введите стороны прямоугольника");
+                            Console.Write("a=");
+                            int firstSide = int.Parse(Console.ReadLine());
+                            Console.Write("b=");
+                            int secondSide = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Кол-во больших квадратов = " + Square.Squares(firstSide, secondSide));
+                            Console.WriteLine("Кол-во маленьких квадратов = " + firstSide * secondSide);
+                        }
+                        break;
+
+                    //В 20
+                    case 6:// K1  Треугоьник(В1)  1.	площадь равностороннего треугольника, периметр которого равен p;
+                        {
+                            Console.Write("Введите периметр треугольника p=");
+                            int P = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Площадь равностороннего треугольника = " + RightTriangle.Area(P));
+                        }
+                        break;
+
+                    case 7: //К2    1.	является ли заданное целое число четным;
+                        {
+                            Console.Write("Является ли заданное целое число четным.\n введите число n=");
+                            int num = int.Parse(Console.ReadLine());
+                            Console.WriteLine(EvenNumber.Check(num));
+                        }
+                        break;
+
+                    case 8://К3 20. Проверка принадлежности точки фигуре
+                        {
+                            Figure.FigureCheck();
+                        }
+                        break;
+
+                    case 9://К4 1.	Дан порядковый номер месяца, вывести на экран количество месяцев оставшихся до конца года.
+                        {
+                            Months.ToEndOfYear();
+                        }
+                        break;
+
+                    case 10: //К5 4. таблица перевода расстояний в дюймах в сантиметры для значений 2, 4, 6, …, 12 дюймов (1 дюйм = 25.4 мм);
+                        {
+                            Units.Convert();
+                        }
+                        break;
+
+                    case 11: //К6 20 печать по шаблону
+                        {
+                            TrianglePrint.Print();
+                        }
+                        break;
+
+
+                }
+
+                PrintMenu();
+                button = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+
+   
+}
+
+
+
